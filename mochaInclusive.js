1f0739cf4547e0bc9189a43600452e6e236306e7
Fina Afifana <finaafifana09@gmail.com>
2021-09-27 11:26:27 +0700

    Create mochaInclusive.js

---
+++ mochaInclusive.js
@@ -0,0 +1,12 @@
+
+var assert = require('assert');
+
+describe('Mathematical Operations - Test Suite', function(){
+    var a = 10;
+    var b = 10;
+
+    it.skip('Addition of two numbers', function(done){
+        var c = a+b;
+        assert.equal(c,20);
+    });
+});
